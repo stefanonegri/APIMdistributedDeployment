92f7dd7356f912240b0c2db755cdd69272a250ab
stefanonegri <39225773+stefanonegri@users.noreply.github.com>
2020-12-23 09:50:59 +0100

    Update install APIM.sh

--- install APIM.sh
+++ install APIM.sh
@@ -6,7 +6,6 @@ apt install openjdk-8-jre-headless
 exit
 scp /Users/stefanonegri/DistributedAPIM/resources/mysql-connector-java-8.0.11.jar root@$APIM_IP:/usr/local/wso2
 ssh root@$APIM_IP
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 cd /usr/local/wso2
 wget --no-check-certificate -r 'https://docs.google.com/uc?export=download&id=1eGNb0SybLTbIuSxHZF_N4tEPwk1zPeAZ' -O wso2am-3.2.0.zip
 unzip wso2am-3.2.0.zip
