6437b1a1159fbfe2bdec4b54a3ae312cffb3b872
stefanonegri <39225773+stefanonegri@users.noreply.github.com>
2020-12-23 15:28:46 +0100

    Add files via upload

---
+++ env.sh
@@ -0,0 +1,7 @@
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export WSO2_HOME=/usr/local/wso2/wso2am-3.2.0/
+export APIM_IP=157.90.31.84
+export TM_IP=168.119.122.183
+export KM_IP=157.90.21.168
+export GTW_IP=159.69.108.75
+export DB_IP=78.47.88.112
